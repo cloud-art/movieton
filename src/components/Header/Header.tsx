68933539fbe469e8f8ac0666f3d4a60acee3f766
Kirart <artyushenko.k.r1@gmail.com>
2022-10-27 19:56:42 +0300

    added hook for close dropdown when user click outside the dropdown box

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,15 +4,20 @@ import classNames from 'classnames';
 import s from './Header.module.scss';
 import Dropdown from './components/Dropdown';
 import { useActions } from '../../hooks/useActions';
+import { useRef } from 'react';
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 interface HeaderProps {}
 
 const Header: React.FunctionComponent<HeaderProps> = () => {
     const { toggleMenu } = useActions();
+    const dropDownRef = useRef<HTMLDivElement>(null);
+    useOnClickOutside(dropDownRef, () => toggleMenu(false));
+
     return (
         <header className={s.Header}>
             <div className={classNames(s.container, 'container')}>
-                <div className={s.left}>
+                <div ref={dropDownRef} className={s.left}>
                     <Burger />
                     <Dropdown />
                     <Logo />
