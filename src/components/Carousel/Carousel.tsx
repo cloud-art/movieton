12bbbad4c468bf139412f3f0d7cd4f4f4d78ceab
artclouds <artyushenko.k.r1@gmail.com>
2023-03-20 14:08:37 +0300

    created carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,7 +1,8 @@
-import React, { cloneElement, FC, PropsWithChildren, ReactNode, useEffect, Children, DetailedReactHTMLElement, ReactElement } from 'react'
+import React, { cloneElement, FC, PropsWithChildren, ReactNode, useEffect, Children, DetailedReactHTMLElement, ReactElement, MutableRefObject } from 'react'
 import s from './Carousel.module.scss'
 import { useState } from 'react'
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
+import { useRef } from 'react'
 
 interface CarouselProps {
     children: ReactNode
@@ -12,26 +13,27 @@ const PAGE_WIDTH = 450
 export const Carousel:FC<CarouselProps> = ({children}) => {
     const [countPages, setCountPages] = useState(React.Children.count(children))
     const [offset, setOffset] = useState(0)
+    const windowRef = useRef() as MutableRefObject<HTMLInputElement>
 
     const handleLeftArrowClick = () => {
         setOffset((offset) => {
             const newOffset = offset + PAGE_WIDTH
             return Math.min(newOffset, 0)
         })
-        console.log(offset)
     }
 
     const handleRightArrowClick = () => {
         setOffset((offset) => {
             const newOffset = offset - PAGE_WIDTH
-            const maxOffset = -(PAGE_WIDTH * (countPages - 1))
+            var windowWidth = windowRef.current.offsetWidth
+            var maxOffset 
+            PAGE_WIDTH * countPages < windowWidth? maxOffset = 0 : maxOffset = windowWidth - (PAGE_WIDTH * countPages)
             return Math.max(newOffset, maxOffset)
         })
-        console.log(offset)
     }
 
     return (
-        <div className={s.container}>
+        <div className={s.container} ref={windowRef}>
             <FaChevronLeft className={s.arrow} onClick={handleLeftArrowClick}/>
             <div 
                 className={s.window}
